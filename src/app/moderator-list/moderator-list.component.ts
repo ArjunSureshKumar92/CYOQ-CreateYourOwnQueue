caa5494253aa6d6e760e0834e7a17cc5879839c0
LeicaSimile <leicasimile@gmail.com>
2019-03-16 01:17:30 -0400

    Right align add mod button

--- src/app/moderator-list/moderator-list.component.ts
+++ src/app/moderator-list/moderator-list.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-moderator-list',
   template: `
-  <button routerLink="/moderator/create" class="btn btn-primary">+ Add Moderator</button>
+  <div class="text-right">
+    <button routerLink="/moderator/create" class="btn btn-primary">+ Add Moderator</button>
+  </div>
   `
 })
 export class ModeratorListComponent implements OnInit {
