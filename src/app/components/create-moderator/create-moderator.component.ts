48a1ce25808556359cd273ce8aff76db3b9fc3cd
LeicaSimile <leicasimile@gmail.com>
2019-03-19 21:36:22 -0400

    Add form submission function

--- src/app/components/create-moderator/create-moderator.component.ts
+++ src/app/components/create-moderator/create-moderator.component.ts
@@ -5,25 +5,27 @@ import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 @Component({
   selector: 'app-create-moderator',
   template: `
-  <form>
+  <form [formGroup]="modForm" (ngSubmit)="submit()">
     <button type="button" (click)="close()" class="close" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
     <div class="form-group">
         <label for="name">Name</label>
-        <input type="text" class="form-control form-control-lg" id="name" name="name" placeholder="Full name" />
+        <input type="text" class="form-control form-control-lg" formControlName="name" placeholder="Full name" />
     </div>
     <div class="form-group">
         <label for="email">Email address</label>
-        <input type="text" class="form-control" id="email" name="email" placeholder="mail@email.com" />
+        <input type="text" class="form-control" formControlName="email" placeholder="mail@email.com" />
     </div>
-    <input type="submit" class="btn btn-primary btn-block btn-lg" value="Create Moderator" />
+    <input type="submit" class="btn btn-primary btn-block btn-lg" [disabled]="modForm.pristine || modForm.invalid" value="Create Moderator" />
   </form>
   `
 })
 export class CreateModeratorComponent implements OnInit {
-
-    constructor(private location: Location) { }
+    modForm: FormGroup;
+    constructor(private location: Location, private fb: FormBuilder) {
+        this.createForm();
+    }
 
     ngOnInit() {
     }
@@ -32,4 +34,14 @@ export class CreateModeratorComponent implements OnInit {
         this.location.back();
     }
 
+    createForm() {
+        this.modForm = this.fb.group({
+            name: ['', Validators.required],
+            email: ['', Validators.required]
+        });
+    }
+
+    submit() {
+
+    }
 }
