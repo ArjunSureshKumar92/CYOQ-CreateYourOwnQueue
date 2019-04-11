e8b773f6a43dcb1dd69145368328b4f7f19b1e68
LeicaSimile <leicasimile@gmail.com>
2019-04-11 19:48:49 -0400

    Separate comoponents according to user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,23 @@ import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AdminHomeComponent } from './components/admin-home/admin-home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { CreateQueueComponent } from './components/create-queue/create-queue.component';
-import { CreateModeratorComponent } from './components/create-moderator/create-moderator.component';
-import { ModeratorListComponent } from './components/moderator-list/moderator-list.component';
-import { ModeratorItemComponent } from './components/moderator-item/moderator-item.component';
-import { ModeratorDetailsComponent } from './components/moderator-details/moderator-details.component';
-import { CompanyDetailsComponent } from './components/company-details/company-details.component';
-import { CompanyListComponent } from './components/company-list/company-list.component';
-import { CompanyItemComponent } from './components/company-item/company-item.component';
-import { QueueListComponent } from './components/queue-list/queue-list.component';
-import { QueueItemComponent } from './components/queue-item/queue-item.component';
-import { QueueDetailsComponent } from './components/queue-details/queue-details.component';
+import { NavbarComponent } from './components/admin/navbar/navbar.component';
+import { AdminHomeComponent } from './components/admin/admin-home/admin-home.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { CreateQueueComponent } from './components/admin/create-queue/create-queue.component';
+import { CreateModeratorComponent } from './components/admin/create-moderator/create-moderator.component';
+import { ModeratorListComponent } from './components/admin/moderator-list/moderator-list.component';
+import { ModeratorItemComponent } from './components/admin/moderator-item/moderator-item.component';
+import { CompanyDetailsComponent } from './components/admin/company-details/company-details.component';
+import { CompanyListComponent } from './components/admin/company-list/company-list.component';
+import { CompanyItemComponent } from './components/admin/company-item/company-item.component';
+import { QueueListComponent } from './components/admin/queue-list/queue-list.component';
+import { QueueItemComponent } from './components/admin/queue-item/queue-item.component';
 import { FilterPipe } from './filter.pipe';
-import { EndUserHomeComponent } from './components/enduser-home/enduser-home.component';
-import { ModeratorHomeComponent } from './components/moderator-home/moderator-home.component';
-import { EndUserListComponent } from './components/end-user-list/end-user-list.component';
-import { TicketItemComponent } from './components/ticket-item/ticket-item.component';
+import { EndUserHomeComponent } from './components/end-user/enduser-home/enduser-home.component';
+import { ModeratorHomeComponent } from './components/moderator/moderator-home/moderator-home.component';
+import { EndUserListComponent } from './components/moderator/end-user-list/end-user-list.component';
+import { TicketItemComponent } from './components/moderator/ticket-item/ticket-item.component';
 
 @NgModule({
   declarations: [
@@ -36,11 +34,9 @@ import { TicketItemComponent } from './components/ticket-item/ticket-item.compon
     CreateQueueComponent,
     CreateModeratorComponent,
     ModeratorListComponent,
-    ModeratorDetailsComponent,
     CompanyDetailsComponent,
     QueueListComponent,
     QueueItemComponent,
-    QueueDetailsComponent,
     ModeratorItemComponent,
     CompanyListComponent,
     CompanyItemComponent,
