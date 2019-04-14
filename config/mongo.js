024538fc4c609ea04ddecc1f2d0b1110b5964e21
devgitk <sridhara.keshav@gmail.com>
2019-04-13 23:28:43 -0400

    mlab

--- config/mongo.js
+++ config/mongo.js
@@ -1,1 +1,3 @@
-exports.connectionName = 'mongodb://localhost:27017/Centennial';
+//exports.connectionName = 'mongodb://localhost:27017/Centennial';
+
+exports.connectionName = 'mongodb://cyoq:cyoqsdp1@ds139896.mlab.com:39896/cyoq';
