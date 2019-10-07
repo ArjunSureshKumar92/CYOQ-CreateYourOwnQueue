0c9dee2a0f3d13519f2f58e1f3d665ba7047f217
LeicaSimile <leicasimile@gmail.com>
2019-10-07 18:45:53 -0400

    Consolidate controllers

--- routes/moderator.js
+++ routes/moderator.js
@@ -1,43 +1,41 @@
 var express = require('express');
 var router = express.Router();
-var updateTicketController = require('../controller/updateTicket');
-var getModeratorController = require('../controller/getModerator');
-var getTicketController = require('../controller/getTicket');
-var getQueueController = require('../controller/getQueue');
+var ticketController = require('../controller/ticket');
+var moderatorController = require('../controller/moderator');
+var queueController = require('../controller/queue');
 
 
 /**
  * All the routes that are starting with api. 
  */
-
 //Get cases
 router.get('/:authKey/queue/get/:companyId/:queueId', function (req, res) {
-  getQueueController.getQueue(req,res);
+  queueController.getQueue(req,res);
 });
 
 router.get('/:authKey/queue/get/:companyId', function (req, res) {
-  getQueueController.getModeratorRelatedQueue(req,res);
+  queueController.getModeratorRelatedQueue(req,res);
 });
 
 router.get('/:authKey/ticket/active/:companyId/:queueId', function (req, res) {
-  getTicketController.getActiveTicket(req,res);
+  ticketController.getActiveTicket(req,res);
 });
 
 router.get('/:authKey/ticket/wait/:companyId/:queueId', function (req, res) {
-  getTicketController.getWaitTicket(req,res);
+  ticketController.getWaitTicket(req,res);
 });
 
 router.get('/:authKey/ticket/get/:companyId/:queueId/:ticketId', function (req, res) {
-  getTicketController.getTicket(req,res);
+  ticketController.getTicket(req,res);
 });
 
 //Update cases
 router.put('/:authKey/ticket/close', function (req, res) {
-  updateTicketController.closeTicket(req,res);
+  ticketController.closeTicket(req,res);
 });
 
 router.put('/:authKey/ticket/next', function (req, res) {
-  updateTicketController.nextTicket(req,res);
+  ticketController.nextTicket(req,res);
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var createTicketController = require('../controller/createTicket');
-var getTicketController = require('../controller/getTicket');
-var deleteTicketController = require('../controller/deleteTicket');
-
-
+var ticketController = require('../controller/ticket');
 
 /**
  * All the routes that are starting with api. 
  */
-
 //Create cases
 router.post('/ticket/create', function (req, res) {
-  createTicketController.createTicket(req,res);
+  ticketController.createTicket(req,res);
 });
 
 //Delete cases
 router.delete('/:authKey/ticket/delete', function (req, res) {
-  deleteTicketController.deleteTicket(req,res);
+  ticketController.deleteTicket(req,res);
 });
 
 //Get cases
 
 router.get('/:authKey/ticket/get/:companyId/:queueId/:ticketId', function (req, res) {
-  getTicketController.getTicket(req,res);
+  ticketController.getTicket(req,res);
 });
 
 router.get('/:authKey/ticket/getposition/:companyId/:queueId/:ticketId', function (req, res) {
-  getTicketController.getTicketPosition(req,res);
+  ticketController.getTicketPosition(req,res);
 });
 
 module.exports = router;
