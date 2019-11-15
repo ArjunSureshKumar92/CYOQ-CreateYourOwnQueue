ed3d26ab7929c041efe5f75187f6e023c18ead36
arjunsureshkumar92 <akumar@emailtopia.com>
2019-11-14 23:10:46 -0500

    changed mongo

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,5 @@
 // Mongo connection string
 
-exports.connectionName = 'mongodb://localhost:27017/Centennial';
+//exports.connectionName = 'mongodb://localhost:27017/Centennial';
 
-//exports.connectionName = 'mongodb://cyoq:cyoq2019@ds331548.mlab.com:31548/cyoq';
+exports.connectionName = 'mongodb://cyoq:cyoq2019@ds331548.mlab.com:31548/cyoq';
