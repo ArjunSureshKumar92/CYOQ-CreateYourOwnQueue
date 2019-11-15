7938bf2915464a56586b37a0a3a35a323225a7bc
arjunsureshkumar92 <akumar@emailtopia.com>
2019-11-14 21:36:40 -0500

    without angular apis working

--- src/app/models/moderator.model.ts
+++
@@ -1,8 +0,0 @@
-export class Moderator {
-    constructor(
-        public name: String,
-        public moderatorId: String,
-        public companyId: String,
-        public email: String
-    ) {}
-}
