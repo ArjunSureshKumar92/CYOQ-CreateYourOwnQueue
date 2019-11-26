f196508fbe7826a30555880e4da62c01a1174c24
rjlucky247s <rjlucky247s@gmail.com>
2019-11-26 17:37:24 -0500

    build fixes

--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var ticketController = require('../controller/ticket');
+var ticketController = require("../controller/ticket");
 
 /**
- * All the routes that are starting with api. 
+ * All the routes that are starting with api.
  */
 //Create cases
-router.post('/ticket/create', function (req, res) {
-  ticketController.createTicket(req,res);
+router.post("/ticket/create", function(req, res) {
+  ticketController.createTicket(req, res);
 });
 
 //Delete cases
-router.delete('/:authKey/ticket/delete', function (req, res) {
-  ticketController.deleteTicket(req,res);
+router.delete("/:authKey/ticket/delete", function(req, res) {
+  console.log(req.body);
+  ticketController.deleteTicket(req, res);
 });
 
 //Get cases
 
-router.get('/:authKey/ticket/get/:companyId/:queueId/:ticketId', function (req, res) {
-  ticketController.getTicket(req,res);
+router.get("/:authKey/ticket/get/:companyId/:queueId/:ticketId", function(
+  req,
+  res
+) {
+  ticketController.getTicket(req, res);
 });
 
-router.get('/:authKey/ticket/getposition/:companyId/:queueId/:ticketId', function (req, res) {
-  ticketController.getTicketPosition(req,res);
-});
+router.get(
+  "/:authKey/ticket/getposition/:companyId/:queueId/:ticketId",
+  function(req, res) {
+    ticketController.getTicketPosition(req, res);
+  }
+);
 
-router.get('/:authKey/queue/get/:companyId', function (req, res) {
-  ticketController.getUserRelatedQueueTicket(req,res);
+router.get("/:authKey/queue/get/:companyId", function(req, res) {
+  ticketController.getUserRelatedQueueTicket(req, res);
 });
 
 module.exports = router;
